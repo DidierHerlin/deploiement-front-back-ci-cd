782d9c9e282725c6c8acf6e6209fa6663b6954a9
DidierHerlin <didierherlin18@gmail.com>
2026-09-18 17:12:06 +0300

    fix(backend): add default SECRET_KEY to prevent docker crash

--- Back-end/configuration/settings.py
+++ Back-end/configuration/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ─── Sécurité ────────────────────────────────────────────────────────────────
 # https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-cle-par-defaut-pour-le-dev")
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
 
