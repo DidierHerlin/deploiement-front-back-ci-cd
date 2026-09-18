71201cab9b6cd52f3100a41518b9922f49cf8019
DidierHerlin <didierherlin18@gmail.com>
2026-09-18 17:21:48 +0300

    fix(backend): add default DB values to prevent docker crash

--- Back-end/configuration/settings.py
+++ Back-end/configuration/settings.py
@@ -76,9 +76,9 @@ CORS_EXPOSE_HEADERS = ["Content-Disposition"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
+        "NAME": config("DB_NAME", default="gesion_immobilier_back_end"),
+        "USER": config("DB_USER", default="postgres"),
+        "PASSWORD": config("DB_PASSWORD", default=""),
         "HOST": config("DB_HOST", default="localhost"),
         "PORT": config("DB_PORT", default="5432"),
     }
