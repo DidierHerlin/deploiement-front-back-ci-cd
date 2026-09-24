2e38f2e2183b545fe226b7f67970eb9bcb047bdf
DidierHerlin <didierherlin18@gmail.com>
2026-09-24 10:22:03 +0300

    correction pdf

---
+++ Front/fix_quittance.mjs
@@ -0,0 +1,5 @@
+﻿import fs from 'fs';
+let s = fs.readFileSync('../Back-end/configuration/paiement/templates/paiement/quittance_template.html', 'utf8');
+s = s.replace(/<table class="grid" role="presentation">[\s\S]*?<th([^>]+)>([\s\S]*?)<\/th>[\s\S]*?<th([^>]+)>([\s\S]*?)<\/th>[\s\S]*?<\/table>/, 
+  '<table class="grid" role="presentation">\n    <tr>\n      <td></td>\n      <td></td>\n    </tr>\n  </table>');
+fs.writeFileSync('../Back-end/configuration/paiement/templates/paiement/quittance_template.html', s, 'utf8');
---
+++ Front/fix_quittance_proper.mjs
@@ -0,0 +1,13 @@
+﻿import fs from 'fs';
+let s = fs.readFileSync('../Back-end/configuration/paiement/templates/paiement/quittance_template.html', 'utf8');
+
+let startIndex = s.indexOf('<table class="grid" role="presentation">');
+let endIndex = s.indexOf('</table>', startIndex);
+
+let tableBlock = s.substring(startIndex, endIndex);
+tableBlock = tableBlock.replace(/<th/g, '<td');
+tableBlock = tableBlock.replace(/<\/th>/g, '</td>');
+
+s = s.substring(0, startIndex) + tableBlock + s.substring(endIndex);
+
+fs.writeFileSync('../Back-end/configuration/paiement/templates/paiement/quittance_template.html', s, 'utf8');
